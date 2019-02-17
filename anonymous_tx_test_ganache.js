e5af3501f93bf64154b233638c6c800e5ef0d701
Igor Gulamov <igor.gulamov@gmail.com>
2019-02-17 16:54:39 +0300

    move snarks to separate process

--- anonymous_tx_test_ganache.js
+++ anonymous_tx_test_ganache.js
@@ -10,6 +10,9 @@ const fs = require("fs");
 const crypto = require("crypto");
 const bigInt = require("big-integer");
 const {stringifyBigInts, unstringifyBigInts} = require("./src/stringifybigint.js");
+const {spawn} = require('child_process');
+
+
 
 const alt_bn_128_q = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 
@@ -80,7 +83,12 @@ function addrToInt(addr) {
   return bigInt(addr.substr(2), "16").value;
 }
 
+
+const depositProver = spawn("node", ["depositProver.js"]);
+depositProver.unref();
+
 async function makeDeposit(contract, _account, _balance) {
+
   const account = addrToInt(_account);
   const balance = BigInt(_balance);
   const salt = rbigint(14);
@@ -89,8 +97,8 @@ async function makeDeposit(contract, _account, _balance) {
     salt: salt,
     owner: account
   };
-  const hash = serializeAndHashUTXO(tx);
 
+  const hash = serializeAndHashUTXO(tx);
   const input = {
     balance: balance,
     salt: salt,
@@ -98,13 +106,17 @@ async function makeDeposit(contract, _account, _balance) {
     hash: hash
   };
 
+  depositProver.stdin.write(JSON.stringify(stringifyBigInts(input)));
+  const [pi_a, pi_b, pi_c, pubinputs] = JSON.parse(await new Promise(resolve => depositProver.stdout.on('data', resolve)));
+  console.log(JSON.stringify([pi_a, pi_b, pi_c, pubinputs]));
+
+
   // console.log(JSON.stringify(stringifyBigInts(input)));
   // console.log(p256(hash));
   // console.log(await contract.methods.utxo(p256(hash)).call());
 
 
-  const witness = sDeposit.circuit.calculateWitness(input);
-  const {proof, publicSignals} = groth.genProof(sDeposit.vk_proof, witness);
+
 
   // console.log(JSON.stringify([[p256(proof.pi_a[0]), p256(proof.pi_a[1])], [[p256(proof.pi_b[0][1]), p256(proof.pi_b[0][0])], [p256(proof.pi_b[1][1]), p256(proof.pi_b[1][0])]],
   // [p256(proof.pi_c[0]), p256(proof.pi_c[1])], publicSignals.map(p256)]));
@@ -113,14 +125,16 @@ async function makeDeposit(contract, _account, _balance) {
   //   [[p256(proof.pi_b[0][1]), p256(proof.pi_b[0][0])], [p256(proof.pi_b[1][1]), p256(proof.pi_b[1][0])]], 
   //   [p256(proof.pi_c[0]), p256(proof.pi_c[1])], publicSignals.map(p256)).send({value:_balance, from:_account, gas:"2000000", gasprice:"2000000000"});
   
-  for(let i = 1; i <10; i++) { console.log(i);
-  const [pi_a, pi_b, pi_c, pubinputs] = [["0x16ad130715d3099719c185872eb920251a4a2ddd3579ea41b5538688bda228a4","0x282c07197266a7932098dcf841ecf3c9155145573acb7c5ef7ec8b45da4aef11"],[["0x1f298e0e5f8a410d16788e9200e804e4c085947cae8f7685d0648c825a93983d","0x22303cfad346fadefe0540736f9980530d1309e3f9cb4d6748aac96601ce63c1"],["0x201bf9e0c04855ed2030e1ec530c0cae46a8f180f69f6d5ffcc25a4253b5245c","0x15ecb92480aa9efbc1c9717aaa4706adf4f0d10851a5d22c3baa110951afd1b4"]],["0x0ebc08c0fd017d5732ea8621a6d8bef187593b3c02a652c7703e8f95d2ec9a8b","0x0561c7998d5b79415abbc3b2cfe8836ea22021cfb8ce99251f55f68c49876688"],["0x000000000000000000000000000000000000000000000000002386f26fc10000","0x0000000000000000000000003b42407cd0c0166476ff8601adbe915990eaf026","0x2dc89d7e4a479085f32fbdcb0dcb03d3f5c7d368232cd4acc2677d3b3e0fdd9e"]];
 
+  //const [pi_a, pi_b, pi_c, pubinputs] = [["0x16ad130715d3099719c185872eb920251a4a2ddd3579ea41b5538688bda228a4","0x282c07197266a7932098dcf841ecf3c9155145573acb7c5ef7ec8b45da4aef11"],[["0x1f298e0e5f8a410d16788e9200e804e4c085947cae8f7685d0648c825a93983d","0x22303cfad346fadefe0540736f9980530d1309e3f9cb4d6748aac96601ce63c1"],["0x201bf9e0c04855ed2030e1ec530c0cae46a8f180f69f6d5ffcc25a4253b5245c","0x15ecb92480aa9efbc1c9717aaa4706adf4f0d10851a5d22c3baa110951afd1b4"]],["0x0ebc08c0fd017d5732ea8621a6d8bef187593b3c02a652c7703e8f95d2ec9a8b","0x0561c7998d5b79415abbc3b2cfe8836ea22021cfb8ce99251f55f68c49876688"],["0x000000000000000000000000000000000000000000000000002386f26fc10000","0x0000000000000000000000003b42407cd0c0166476ff8601adbe915990eaf026","0x2dc89d7e4a479085f32fbdcb0dcb03d3f5c7d368232cd4acc2677d3b3e0fdd9e"]];
+  //console.log([hash, tx, pi_a, pi_b, pi_c, pubinputs]);
   //const [pi_a, pi_b, pi_c, pubinputs] = [[p256(proof.pi_a[0]), p256(proof.pi_a[1])], [[p256(proof.pi_b[0][1]), p256(proof.pi_b[0][0])], [p256(proof.pi_b[1][1]), p256(proof.pi_b[1][0])]], [p256(proof.pi_c[0]), p256(proof.pi_c[1])], [p256(publicSignals[0]), p256(publicSignals[1]), p256(publicSignals[2])]];
-  await contract.methods.deposit(pi_a, pi_b, pi_c, pubinputs).send({value:_balance, from:_account, gas:"6000000", gasprice:"2000000000"});
-  }
-  console.log(await contract.methods.utxo(p256(hash)).call());
 
+  try{
+  await contract.methods.deposit(pi_a, pi_b, pi_c, pubinputs).send({value:_balance, from:_account, gas:"6000000", gasprice:"2000000000"});
+  } catch(e){console.error(e);}
+  console.log(await contract.methods.utxo(p256(hash)).call());
+  console.log("#");
 
   
 
@@ -133,7 +147,7 @@ const main = async () => {
   const ozDai = fload("smart-contract/build/contracts/ZDai.json");
   const zDai = new web3.eth.Contract(ozDai.abi, ozDai.networks["7555"].address);
   await makeDeposit(zDai, accounts[0], web3.utils.toWei("0.01"));
-
+  //await makeDeposit(zDai, accounts[0], web3.utils.toWei("0.02"));
 };
 
 main();
